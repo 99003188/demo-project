abfde175fd2802281e21d91bbd73ecfb7a83f965
99003138 <75420647+99003138@users.noreply.github.com>
2020-12-06 00:16:22 +0530

    Create calculator_operations.h

---
+++ SRC/calculator_operations.h
@@ -0,0 +1,41 @@
+#include<calculator_operations.h>
+int sqaure()
+{
+    int side;
+    printf("Enter the side");
+    scanf("%d",&side);
+    return(side*side);
+}
+int rectangle()
+{
+    int length,breadth;
+    printf("Enter the length and breadth");
+    scanf("%d%d",&length,&breadth);
+    return(length*breadth);
+}
+double cone()
+{
+    int radius,heigth;
+    printf("Enter the height and radius");
+    scanf("%d%d",&radius,&heigth);
+    return((1/3)*PI*radius*radius*height);
+    
+}
+double  cylinder
+{
+    int radius,heigth;
+    printf("Enter the height and radius");
+    scanf("%d%d",&radius,&heigth);
+    return(PI*radius*radius*height);
+}
+float BMI()
+{
+    int mass,height;
+    printf("Enter the weight(in Kgs) and height(in meters");
+    scanf("%d,d",&mass,&height)
+    if(height>0)
+    return("mass/(height*height)");
+    else if(height<=0)
+        printf("enter correct details");
+}
+
